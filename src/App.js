fa6f245761dbcec61bb12c7e7a4c54feeaaff64a
SimonGuillaumin <simon.guillaumin@live.fr>
2020-12-01 23:28:53 +0100

    Test

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ThemeSwitcher from './components/ThemeSwitcher.js';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <ThemeSwitcher/>
+     
+
+
+    </div>
+  );
+}
+
+export default App;
